1e57e91f799415defb4a7341a64c30775ccf8791
namsyvo <vosynam@gmail.com>
2018-07-06 18:44:46 +0000

    Use GDC aliquot_id for sample_id

--- modify_purecn_outputs.py
+++ modify_purecn_outputs.py
@@ -6,25 +6,26 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='Modify purecn input files.')
+parser.add_argument('--aliquot_id', required=True, help='GDC aliquot_id')
 parser.add_argument('--info_file', required=True, help='purecn info file')
 parser.add_argument('--seg_file', required=True, help='purecn dnacopy seg file')
 parser.add_argument('--modified_info_file', required=True, help='modified purecn info file')
 parser.add_argument('--modified_seg_file', required=True, help='modified purecn dnacopy seg file')
 args = vars(parser.parse_args())
 
+aliquot_id = args['aliquot_id'].strip()
+
 f1 = open(args['info_file'])
 f2 = open(args['modified_info_file'], "w")
 tmp = f1.readline().strip().split(",")
 f2.write("\t".join(tmp) + "\n")
 tmp = f1.readline().strip().split(",")
-tmp1 = tmp[0].replace(".", "-")
-f2.write("\t".join([tmp1] + tmp[1:]) + "\n")
+f2.write("\t".join([aliquot_id] + tmp[1:]) + "\n")
 f2.close()
 f1.close()
 
 f = open(args['modified_seg_file'], "w")
 for line in open(args['seg_file']):
     tmp = line.strip().split(",")
-    tmp1 = tmp[0].replace(".", "-")
-    f.write("\t".join([tmp1] + tmp[1:]) + "\n")
+    f.write("\t".join([aliquot_id] + tmp[1:]) + "\n")
 f.close()
