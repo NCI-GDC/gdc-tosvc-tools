50c6ce62e9c2c6a505e007acc9657557dd2e3151
Charles Czysz <czysz@uchicago.edu>
2023-02-27 09:34:26 -0600

    Delete old files

--- build_push_dockerhub.sh
+++
@@ -1,2 +0,0 @@
-docker build -t namsyvo/gdc_tosvc_tools:$1 .
-docker push namsyvo/gdc_tosvc_tools:$1
--- test_filter_mutect_outputs.sh
+++
@@ -1,1 +0,0 @@
-python filter_mutect_outputs.py -i /mnt/data/DLBCL/tumors/07182018-test-DLBCL-Nam.mutect2.tumor_only.contFiltered.oxogFiltered.vcf -o test_filter_mutect_outputs.vcf
